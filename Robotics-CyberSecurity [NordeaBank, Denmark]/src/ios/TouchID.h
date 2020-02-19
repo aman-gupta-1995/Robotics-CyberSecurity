8c4574e096aaa061994303efdf0ceeca211071b5
aman-gupta-1995 <amangupta1995.ag@gmail.com>
2020-02-20 02:38:07 +0530

    Add files via upload

---
+++ Robotics-CyberSecurity [NordeaBank, Denmark]/src/ios/TouchID.h
@@ -0,0 +1,15 @@
+#import <Cordova/CDVPlugin.h>
+
+@interface TouchID :CDVPlugin
+
+- (void) isAvailable:(CDVInvokedUrlCommand*)command;
+
+- (void) didFingerprintDatabaseChange:(CDVInvokedUrlCommand*)command;
+
+- (void) verifyFingerprint:(CDVInvokedUrlCommand*)command;
+- (void) verifyFingerprintWithCustomPasswordFallback:(CDVInvokedUrlCommand*)command;
+- (void) verifyFingerprintWithCustomPasswordFallbackAndEnterPasswordLabel:(CDVInvokedUrlCommand*)command;
+
+- (void) askPassword:(CDVInvokedUrlCommand*)command;
+
+@end
